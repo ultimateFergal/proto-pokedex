39fbc0d1f0a25994191823949270a2e126482c2a
Fernando Galarcio <fernandogalarcio@P44-LUISGALARCIO.local>
2021-10-09 11:48:55 -0500

    first commit, setting the pokemin list rendering and routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import AppNavigator from './components/AppNavigator'
+import Pokedex from './containers/Pokedex'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <AppNavigator />
+      <Route path="/" component={Pokedex}></Route>
+    </Router>
+  )
 }
-
-export default App;
