eed1ead32d8077843556cee60946c52e4b09cd39
Fernando Galarcio <fernandogalarcio@P44-LUISGALARCIO.local>
2021-10-10 14:33:13 -0500

    redux implemented, favourites component

---
+++ src/containers/Favourites.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Box, Grid, withStyles } from '@material-ui/core'
+import PokemonCard from '../components/PokemonCard'
+
+const styles = (theme) => ({
+    pokedexContainer: {
+        height: '100vh',
+        backgroundColor: 'rgb(68, 68, 68)',
+        paddingTop: 80,
+        textAlign: 'center',
+    },
+})
+export class Favourites extends Component {
+    render() {
+        const { classes, favourites } = this.props
+        return (
+            <Box>
+                <Grid container spacing={2} className={classes.pokedexContainer}>
+                    {favourites.map((pokemon) => (
+                        <PokemonCard pokemon={pokemon} key={pokemon.id} image={pokemon.sprites.front_default}/>
+                    ))}
+                </Grid>
+            </Box>
+        )
+    }
+}
+
+const mapStateToProps = (state) => ({
+    favourites: state.favourites
+})
+
+const mapDispatchToProps = (dispatch) => {
+    
+}
+
+export default withStyles(styles)(connect(mapStateToProps, mapDispatchToProps)(Favourites))
