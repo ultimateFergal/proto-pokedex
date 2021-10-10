c328f4d6b1eb6042579364642efe5525d58fc18c
Fernando Galarcio <fernandogalarcio@P44-LUISGALARCIO.local>
2021-10-09 19:37:47 -0500

    displaying and routing adjusting, starting details

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import AppNavigator from './components/AppNavigator'
+import PokemonDetails from './components/PokemonDetails'
 import Pokedex from './containers/Pokedex'
 
 export default function App() {
   return (
     <Router>
       <AppNavigator />
-      <Route path="/" component={Pokedex}></Route>
+      <Route exact path="/" component={Pokedex}></Route>
+      <Route exact path="/pokemon/:id" component={PokemonDetails}></Route>
     </Router>
   )
 }
